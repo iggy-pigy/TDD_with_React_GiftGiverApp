b32b39821a894ec0e32af94e90a2220f1988fc16
iggy-pigy <ilgakokorisa@gmail.com>
2020-07-24 22:51:47 +0100

    Added new test to return max number from an array of numbers

--- src/helper/index.test.js
+++ src/helper/index.test.js
@@ -4,6 +4,11 @@ describe('max_number', () => {
     describe('given an empty array', () => {
         it('returns 0', () => {
             expect(max_number([])).toEqual(0);
-        })
-    })
+        });
+    });
+    describe('given an array of numbers', () => {
+        it('returns the max number', () => {
+            expect(max_number([1, 2, 3])).toEqual(3);
+        });
+    });
 });
